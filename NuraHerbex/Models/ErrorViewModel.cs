8b695428dc84958ea0a67caaaabf78343b5b1355
Harini232 <harini.renganathan02@gmail.com>
2025-10-23 18:33:10 +0530

    init

---
+++ NuraHerbex/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NuraHerbex.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
