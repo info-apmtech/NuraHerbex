e57bae2446772e939210c7d73b9f69811c11f23b
Harini232 <harini.renganathan02@gmail.com>
2025-10-25 17:45:32 +0530

    QUIZ PAGE  VIEW ADDED

--- NuraHerbex/Controllers/HomeController.cs
+++ NuraHerbex/Controllers/HomeController.cs
@@ -25,6 +25,10 @@ namespace NuraHerbex.Controllers
         {
             return View();
         }
+        public IActionResult Quiz()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
