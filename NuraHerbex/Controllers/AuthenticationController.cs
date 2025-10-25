9172569c54ccfa30000f94f59782644f8cfa720e
Harini232 <harini.renganathan02@gmail.com>
2025-10-25 10:09:07 +0530

    sigin,signup view completed

---
+++ NuraHerbex/Controllers/AuthenticationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NuraHerbex.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        public IActionResult ForgetPassWord()
+        {
+            return View();
+        }
+    }
+}
