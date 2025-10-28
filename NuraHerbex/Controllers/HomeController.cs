d226bbf50d29ca01f487f8c8490b1bc070263360
AbuApmTech <abu@apmtechnologies.in>
2025-10-28 17:00:09 +0530

    Myreturn page and some css corrections

--- NuraHerbex/Controllers/HomeController.cs
+++ NuraHerbex/Controllers/HomeController.cs
@@ -61,6 +61,10 @@ namespace NuraHerbex.Controllers
         {
             return View();
         }
+        public IActionResult MyReturns()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
