ef37eccd4a87a28c48d8f5a356e099006084c184
AbuApmTech <abu@apmtechnologies.in>
2025-10-28 10:18:16 +0530

    shop, adminSidebar, profile pages

---
+++ NuraHerbex/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NuraHerbex.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
