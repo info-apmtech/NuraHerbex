e25ff3bf36f4b7399665f1aa8690bf497e4f8736
Harini232 <harini.renganathan02@gmail.com>
2025-11-10 17:45:44 +0530

    display order added in product

---
+++ Domain/Migrations/20251110115824_productdisplayorder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class productdisplayorder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DisplayOrder",
+                table: "ProductDetails",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayOrder",
+                table: "ProductDetails");
+        }
+    }
+}
